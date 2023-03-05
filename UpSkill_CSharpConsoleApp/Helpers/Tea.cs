24d294cb83d958c4e16f88de472d115215df6f47
jgogola <jason@gogola.us>
2023-03-05 14:30:24 -0500

    Added an Async/Await example

---
+++ UpSkill_CSharpConsoleApp/Helpers/Tea.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpSkill_CSharpConsoleApp.Helpers
+{
+    public class Tea
+    {
+        public string MakeTea()
+        {
+            var water = BoilWater();
+
+            Console.WriteLine("Take the cups out.");
+
+            Console.WriteLine("Put tea in cups.");
+
+            Console.WriteLine($"Pour {water} in cups");
+
+            return "Tea";
+        }
+
+        private string BoilWater()
+        {
+            Console.WriteLine("Start the kettle");
+
+            Console.WriteLine("Waiting for the kettle to heat.");
+
+            Task.Delay(2000).GetAwaiter().GetResult();
+
+            Console.WriteLine("Kettle finished boiling.");
+
+            return "boiled water";
+        }
+
+
+        public async Task<string> MakeTeaAsync()
+        {
+            var boilingWater = BoilWaterAsync();
+
+            Console.WriteLine("Take the cups out.");
+
+            Console.WriteLine("Put tea in cups.");
+
+            var water = await boilingWater;
+
+            Console.WriteLine($"Pour {water} in cups");
+
+            return "Tea";
+        }
+
+        private async Task<string> BoilWaterAsync()
+        {
+            Console.WriteLine("Start the kettle");
+
+            Console.WriteLine("Waiting for the kettle to heat.");
+
+            await Task.Delay(2000);
+
+            Console.WriteLine("Kettle finished boiling.");
+
+            return "boiled water";
+        }
+    }
+}
--- UpSkill_CSharpConsoleApp/Program.cs
+++ UpSkill_CSharpConsoleApp/Program.cs
@@ -1,19 +1,29 @@
 ﻿
 
+using System.Runtime.CompilerServices;
 using UpSkill_CSharpConsoleApp.Models;
 
 
 /* FlagsEnum Exmple */
-var book = new Book
-{
-    Title = "Frankenstein",
-    Author = "Mary Shelley",
-    AvailableFormats = Format.Hardback | Format.AudioBook
-};
+//var book = new Book
+//{
+//    Title = "Frankenstein",
+//    Author = "Mary Shelley",
+//    AvailableFormats = Format.Hardback | Format.AudioBook
+//};
 
-Console.WriteLine(book.ToString());
+//Console.WriteLine(book.ToString());
 
-if (book.AvailableFormats.HasFlag(Format.AudioBook))
-{
-    Console.WriteLine("Book is available in Audio format!");
-}
+//if (book.AvailableFormats.HasFlag(Format.AudioBook))
+//{
+//    Console.WriteLine("Book is available in Audio format!");
+
+//}
+
+var tea = new UpSkill_CSharpConsoleApp.Helpers.Tea();
+Console.WriteLine(tea.MakeTea());
+
+Console.WriteLine("");
+
+var teaAsync = new UpSkill_CSharpConsoleApp.Helpers.Tea();
+Console.WriteLine(await teaAsync.MakeTeaAsync());
